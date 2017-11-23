bbbf727e3d5e0fd13253450ed4f390cafff4982a
himetani <takafumi_t1224@jcom.home.ne.jp>
2017-11-23 16:24:40 +0900

    tag

--- git/git_tag.go
+++ git/git_tag.go
@@ -1,8 +1,8 @@
 package git
 
 import (
-	"fmt"
 	"strings"
+	"time"
 
 	"github.com/libgit2/git2go"
 )
@@ -15,7 +15,7 @@ type GitTag struct {
 	path string
 }
 
-func (g *GitTag) Analyze(str string) ([]TagCount, error) {
+func (g *GitTag) Analyze(str string, times []time.Time) ([]TagCount, error) {
 	repo, _ := git.OpenRepository(g.path)
 	walk, _ := repo.Walk()
 	err := walk.PushHead()
@@ -23,16 +23,28 @@ func (g *GitTag) Analyze(str string) ([]TagCount, error) {
 		return nil, err
 	}
 
+	tags := []time.Time{}
+
 	repo.Tags.Foreach(func(name string, oid *git.Oid) error {
 		if strings.Contains(name, str) {
 			o, _ := repo.Lookup(oid)
-			t, _ := o.AsTag()
-			c, _ := t.AsCommit()
-			fmt.Println(c)
-			//fmt.Println(c.Summary())
+			tag, _ := o.AsTag()
+			tTime := tag.Tagger().When
+			tags = append(tags, tTime)
 		}
 		return nil
 	})
+
+	tcs := []TagCount{}
+	for _, time := range times {
+		cnt := 0
+		for _, tag := range tags {
+			if tag.After(time) && time.AddDate(0, 1, 0).After(tag) {
+				cnt++
+			}
+		}
+		tcs = append(tcs, TagCount{time: time, cnt: cnt})
+	}
 
 	/*
 		callback := func(c *git.Commit) bool {
@@ -45,5 +57,5 @@ func (g *GitTag) Analyze(str string) ([]TagCount, error) {
 		}
 	*/
 
-	return nil, nil
+	return tcs, nil
 }
--- git/git_tag_test.go
+++ git/git_tag_test.go
@@ -1,14 +1,34 @@
 package git
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+	"time"
+)
 
-func TestGetTime(t *testing.T) {
+func TestAnalyze(t *testing.T) {
 	gt := &GitTag{
-		path: "../glstats-sample",
+		path: "../glstats-sample-submodule",
 	}
 
-	_, err := gt.Analyze("deploy")
+	t0 := time.Date(2017, time.Month(9), 1, 0, 0, 0, 0, time.Local)
+	t1 := time.Date(2017, time.Month(10), 1, 0, 0, 0, 0, time.Local)
+	t2 := time.Date(2017, time.Month(11), 1, 0, 0, 0, 0, time.Local)
+
+	times := []time.Time{t0, t1, t2}
+
+	expected := []TagCount{
+		{time: t0, cnt: 0},
+		{time: t1, cnt: 0},
+		{time: t2, cnt: 1},
+	}
+
+	actual, err := gt.Analyze("deploy", times)
 	if err != nil {
-		t.Logf("Error")
+		t.Fatal("Analyze returnen non-nil\n")
+	}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("Actual was %x\n", actual)
 	}
 }
